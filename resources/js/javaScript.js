31622deea15fdb692d85b79b355378a4ff17d485
saranshlakra <saranshlakra4@gmail.com>
2021-04-28 00:06:43 +0530

    Local storage has been added

--- resources/js/javaScript.js
+++ resources/js/javaScript.js
@@ -1,11 +1,34 @@
 var icon = document.getElementById("dark-mode");
 
-icon.onclick = function () {
+var icon_mobile = document.getElementById("dark-mode-mobile");
+
+// if (localStorage.getItem("theme") == null) {
+//   localStorage.setItem("theme", "light");
+// }
+
+// let localData = localStorage.getItem("theme");
+
+// if (localData == "light") {
+//   icon.src = "resources/css/images/moon-2.png";
+//   document.body.classList.remove("dark-theme");
+// } else if (localData == "dark") {
+//   icon.src = "resources/css/images/sun-2.png";
+//   document.body.classList.add("dark-theme");
+// }
+
+var funClick = function () {
   document.body.classList.toggle("dark-theme");
 
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "sun-2.png";
+    icon.src = "resources/css/images/sun-2.png";
+    icon_mobile.src = "resources/css/images/sun-2.png";
+    localStorage.setItem("theme", "dark");
   } else {
-    icon.src = "moon-2.png";
+    icon.src = "resources/css/images/moon-2.png";
+    icon_mobile.src = "resources/css/images/moon-2.png";
+    localStorage.setItem("theme", "light");
   }
 };
+
+icon.onclick = funClick;
+icon_mobile.onclick = funClick;
