28f0e8304a528d5de131d87e929a9adb429ecdde
saranshlakra <saranshlakra4@gmail.com>
2021-04-28 00:10:34 +0530

    Update javaScript.js

--- resources/js/javaScript.js
+++ resources/js/javaScript.js
@@ -2,19 +2,19 @@ var icon = document.getElementById("dark-mode");
 
 var icon_mobile = document.getElementById("dark-mode-mobile");
 
-// if (localStorage.getItem("theme") == null) {
-//   localStorage.setItem("theme", "light");
-// }
+if (localStorage.getItem("theme") == null) {
+  localStorage.setItem("theme", "light");
+}
 
-// let localData = localStorage.getItem("theme");
+let localData = localStorage.getItem("theme");
 
-// if (localData == "light") {
-//   icon.src = "resources/css/images/moon-2.png";
-//   document.body.classList.remove("dark-theme");
-// } else if (localData == "dark") {
-//   icon.src = "resources/css/images/sun-2.png";
-//   document.body.classList.add("dark-theme");
-// }
+if (localData == "light") {
+  icon.src = "resources/css/images/moon-2.png";
+  document.body.classList.remove("dark-theme");
+} else if (localData == "dark") {
+  icon.src = "resources/css/images/sun-2.png";
+  document.body.classList.add("dark-theme");
+}
 
 var funClick = function () {
   document.body.classList.toggle("dark-theme");
